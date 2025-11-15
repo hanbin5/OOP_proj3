3487b702d4a43af0b45f0e0d447a6aad2564909e
hanbin5 <hanbinftr55@gmail.com>
2025-11-15 20:22:31 +0900

    First Commit

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/hanbinftr55/Coding/oop_proj3/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/meshgen.cpp" "CMakeFiles/main.dir/src/meshgen.cpp.o" "gcc" "CMakeFiles/main.dir/src/meshgen.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/orbit.cpp" "CMakeFiles/main.dir/src/orbit.cpp.o" "gcc" "CMakeFiles/main.dir/src/orbit.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/ply_3dgs.cpp" "CMakeFiles/main.dir/src/ply_3dgs.cpp.o" "gcc" "CMakeFiles/main.dir/src/ply_3dgs.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/prim.cpp" "CMakeFiles/main.dir/src/prim.cpp.o" "gcc" "CMakeFiles/main.dir/src/prim.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/projector_cpu_depthsort.cpp" "CMakeFiles/main.dir/src/projector_cpu_depthsort.cpp.o" "gcc" "CMakeFiles/main.dir/src/projector_cpu_depthsort.cpp.o.d"
+  "/Users/hanbinftr55/Coding/oop_proj3/src/renderer_gl41.cpp" "CMakeFiles/main.dir/src/renderer_gl41.cpp.o" "gcc" "CMakeFiles/main.dir/src/renderer_gl41.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/third_party/tinyply/tinyply.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/third_party/tinyply/tinyply.cpp.o: \
+  /Users/hanbinftr55/Coding/oop_proj3/third_party/tinyply/tinyply.cpp
